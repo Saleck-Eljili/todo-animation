f2d5962ed52a9cf16d559a39c1fa0af04bd463f5
Saleck-Eljili <salek.eljili@gmail.com>
2022-04-07 11:43:11 +0200

    feat(TODO): animation with react traveal

--- src/App.js
+++ src/App.js
@@ -2,11 +2,35 @@ import React from "react";
 import "./App.css";
 import TodoList from "./components/TodoList";
 
+import { useState } from "react";
+import Zoom from "react-reveal/Zoom";
+
+function ZoomExample(props) {
+  let [show, setShow] = useState(false);
+
+  function toggleShow() {
+    setShow(!show);
+  }
+  return (
+    <div className="test">
+      <Zoom when={show}>
+        <h1>React Reveal</h1>
+      </Zoom>
+      <button type="button" onClick={toggleShow}>
+        Click to {show ? "Hide" : "Show"}
+      </button>
+    </div>
+  );
+}
+
 function App() {
   return (
-    <div className="todo-app">
-      <TodoList />
-    </div>
+    <>
+      <ZoomExample />
+      <div className="todo-app">
+        <TodoList />
+      </div>
+    </>
   );
 }
 
